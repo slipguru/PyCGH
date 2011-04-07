16b163727aa2fcf9ef5332fbebb745bd47ddb2ab
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2011-04-07 18:18:00 +0200

    avanzamenti e note

--- cghutils/_arrayCGH.py
+++ cghutils/_arrayCGH.py
@@ -1,5 +1,5 @@
-import warnings
 import numpy as np
+from numpy import ma
 
 # This class represent only a useful way to read and manipulate
 # the values associated with an aCGH
@@ -16,50 +16,109 @@ import numpy as np
 # The standard constructor accept a dictionary with all the needed values
 # The class was thought as a read-only structure
 
+TYPES = [('row', np.int16),             # no missing
+         ('col', np.int16),             # //
+         ('id', np.str_),
+         ('ref_signal', np.float_),
+         ('sample_signal', np.float_),
+         ('ref_bg', np.float_),
+         ('sample_bg', np.float_),
+         ('chromosome', np.int16),
+         ('start_base', np.int_),
+         ('end_base', np.int_)]
 
 class ArrayCGH(object):
-    def __init__(self, input_dict):
 
-        # Set of expected values
-        acgh_keys = set(('row', 'col',
-                         'ref_signal', 'sample_signal',
-                         'ref_bg', 'sample_bg',
-                         'chromosome', 'start_base', 'end_base'))
+    def __init__(self, input_dict, filter=None):
+        # Ordered list to record array
+        names = [k[0] for k in TYPES]
+        buff = [np.asarray(input_dict[k[0]], dtype=k[1]) for k in TYPES]
 
-        # Check if there exist at least one valid list of values
-        valid_keys = list(acgh_keys.intersection(input_dict))
-        if not valid_keys:
-            raise ValueError('input dictionary does not contain expected keys')
-
-        # Check if some lists of values are missing (others will be discarded)
-        missing = acgh_keys.difference(input_dict)
-        if missing:
-            warnings.warn('missing lists of values: %s' % ', '.join(missing),
-                          RuntimeWarning)
-
-        # Array Geometry
-        row_num = np.max(input_dict.get('row', [0]))
-        col_num = np.max(input_dict.get('col', [0]))
-        array_length = (row_num*col_num) or len(input_dict[valid_keys[0]])
-
-        # Data population
-        empty_array = np.array([np.nan]*(array_length))
-        self.metadata = {'Row num': row_num,
-                         'Col num': col_num}
-
-        for key in valid_keys:
-            setattr(self, key, np.asarray(input_dict[key], dtype=float))
-
-        for key in missing:
-            setattr(self, key, self._empty_array)
-
-        # Masked arrays
-        #a = np.array([1, 2, np.nan])
-        #a = [1, 2, None]
-        #b = ma.masked_invalid(np.asarray(a, dtype=float))
-
-    def __getattr__(self, key):
         try:
-            return []#self._internals[key]
-        except KeyError, e:
-            raise AttributeError(e)
+            self._rdata = np.rec.fromarrays(buff, names=names)
+        except ValueError, e:
+            msg, index = str(e).split(' array', 1)
+            raise ValueError("%s '%s'" % (msg, names[int(index)]))
+
+        if filter is None:
+            filter = np.ones(len(self._rdata['row']), dtype=bool)
+        elif len(filter) != len(self._rdata['row']):
+            raise ValueError("invalid mask length")
+        self._filter = filter
+
+        # Utile..... se abbiamo solo una unica maschera
+        # che ce ne facciamo...
+        # Ma mi interessa davvero perdere tutto sto tempo?!?!
+        # Se i dati li passa l'utente li passa completi:
+        # controllo se row e col ci sono tutti altrimenti riempio
+        # e creo la mashera (come add in R).
+        # ritorno sempre delle copie per non modificare l'array
+        # In lettura da file faccio lo stesso, il filling e' solo in presenza
+        # di righe e colonne mancanti, l'utenti non se ne frega dei missing
+        # quelli ce li metto io quando e se servono.
+        # i filtri si fanno con dei metodi che al massimo espendono
+        # la maschera di filtro... basta cosi'
+        # Per R ho dei metodi che mi estraggono i vettori completi
+        # dove l'utente puo' decidere come fare il fill
+        # [key] -> ritorna filtrati
+        # filled(key, fill_values=XX, dtype=Casting) -> fillato....
+        self._rdata = np.ma.masked_array(np.asarray(self._rdata), mask=~filter)
+
+        # possibles optimizations:
+        #   self._rdata = np.asarray(self._rdata)
+        #   self._rdata = self._rdata.view(ndarray)
+
+    # the metod returns a copy
+    def __getitem__(self, key):
+        return self._rdata[key]#[self._filter]
+
+    def compute_ratios(self, bg_subtracted=False):
+        ref_signal = self._rdata['ref_signal']
+        sample_signal = self._rdata['sample_signal']
+
+        if bg_subtracted:
+            ref_signal -= self._rdata['ref_bg']
+            sample_signal -= self._rdata['sample_bg']
+
+        return ma.log2(sample_signal / ref_signal)
+
+    #def __init__(self, input_dict):
+    #
+    #    # Set of expected values
+    #    acgh_keys = set(('row', 'col',
+    #                     'ref_signal', 'sample_signal',
+    #                     'ref_bg', 'sample_bg',
+    #                     'chromosome', 'start_base', 'end_base'))
+    #
+    #    # Check if there exist at least one valid list of values
+    #    valid_keys = list(acgh_keys.intersection(input_dict))
+    #    if not valid_keys:
+    #        raise ValueError('input dictionary does not contain expected keys')
+    #
+    #    # Check if some lists of values are missing (others will be discarded)
+    #    missing = acgh_keys.difference(input_dict)
+    #    if missing:
+    #        warnings.warn('missing lists of values: %s' % ', '.join(missing),
+    #                      RuntimeWarning)
+    #
+    #    # Array Geometry
+    #    row_num = np.max(input_dict.get('row', [0]))
+    #    col_num = np.max(input_dict.get('col', [0]))
+    #    array_length = (row_num*col_num) or len(input_dict[valid_keys[0]])
+    #
+    #    # Data population
+    #    empty_array = np.array([np.nan]*(array_length))
+    #    self.metadata = {'Row num': row_num,
+    #                     'Col num': col_num}
+    #
+    #    for key in valid_keys:
+    #        setattr(self, key, np.asarray(input_dict[key], dtype=float))
+    #
+    #    for key in missing:
+    #        setattr(self, key, self._empty_array)
+    #
+    #    # Masked arrays
+    #    #a = np.array([1, 2, np.nan])
+    #    #a = [1, 2, None]
+    #    #b = ma.masked_invalid(np.asarray(a, dtype=float))
+    #
--- cghutils/test/test_arrayCGH.py
+++ cghutils/test/test_arrayCGH.py
@@ -1,39 +1,73 @@
 from nose.tools import *
 
-import warnings
+import itertools as it
 import numpy as np
 from cghutils import ArrayCGH
 
 class TestArrayCGH(object):
 
-    def test_init(self):
-        with warnings.catch_warnings(record=True) as w:
-            acgh = ArrayCGH({'row': range(11)})
-            assert issubclass(w[-1].category, RuntimeWarning)
+    def setup(self):
+        self.ROW_NUM = self.COL_NUM = 10
+        row, col =  zip(*it.product(range(1, self.ROW_NUM+1),
+                                    range(1, self.COL_NUM+1)))
+        self.input =  {'row': list(row),
+                       'col': list(col),
+                       'id': ['Probe%d' % x for x in range(1, len(row)+1)],
+                       'ref_signal': [10]*len(row),
+                       'sample_signal': [20]*len(row),
+                       'ref_bg': [1]*len(row),
+                       'sample_bg': [2]*len(row),
+                       'chromosome': (range(1, 25)*5)[:len(row)],
+                       'start_base': range(1, len(row)+1),
+                       'end_base': [x+10 for x in range(1, len(row)+1)]
+                    }
 
-        assert_equals(10, acgh.metadata['Row num'])
-        assert_equals(0, acgh.metadata['Col num'])
+    def test_subscription(self):
+        acgh = ArrayCGH(self.input)
+        for k in self.input:
+            assert_equals(len(self.input[k]), len(acgh[k]))
 
-        # At startup all the "columns" of the table are empty
-        # it's like a clean array.
-        assert_equals(0, len(acgh.ref_signal))
-        assert_true(np.all(np.isnan(acgh.ref_signal)))
+    def test_ratios(self):
+        acgh = ArrayCGH(self.input)
+        ratios = acgh.compute_ratios()
 
-    def test_init_with_values(self):
-        pass
+        assert_equals(self.ROW_NUM*self.COL_NUM, len(ratios))
 
-    #def test_wrong_name(self):
-        #assert_raises(AttributeError, getattr, self.acgh, 'foo_signal')
+        expected_ratios = np.log2(acgh['sample_signal'] /
+                                  acgh['ref_signal'])
+        assert_true(np.allclose(expected_ratios, ratios))
 
-        #ref_signal=foo_signal,
-        #sample_signal=foo_signal,
-        #ref_bg=foo_signal,
-        #sample_bg=foo_signal,
-        #chromosomes=[1]*100,
-        #start_base=foo_signal,
-        #end_base=foo_signal)
+    def test_ratios_bg(self):
+        acgh = ArrayCGH(self.input)
+        ratios = acgh.compute_ratios(bg_subtracted=True)
 
-        #for x in (acgh.ref_signal, acgh.sample_signal,
-        #          acgh.ref_bg, acgh.sample_bg):
-        #    assert_equals(100, len(x))
-        #    assert_equals(foo_signal, x)
+        expected_ratios = np.log2(
+                            (acgh['sample_signal'] - acgh['sample_bg']) /
+                            (acgh['ref_signal'] - acgh['ref_bg']))
+        assert_true(np.allclose(expected_ratios, ratios))
+
+    def test_ids(self):
+        acgh = ArrayCGH(self.input)
+        assert_equals(np.dtype('|S8'), acgh['id'].dtype)
+
+    def test_wrong_shape(self):
+        for k in self.input:
+            input = dict(self.input) # copy
+            input[k] = input[k][1:] # removing one element
+
+            try:
+                ArrayCGH(self.input)
+            except ValueError, e:
+                assert_equals("array-shape mismatch in '%s'" % k, str(e))
+
+    def test_filtered_data(self):
+        filter = np.array([True]*len(self.input['row']))
+        filter[10] = False #masking one element
+
+        acgh = ArrayCGH(self.input, filter=filter)
+        for k in self.input:
+            assert_equals(len(self.input[k])-1, len(acgh[k].compressed()))
+
+    #def test_ratios_ordered(self):
+        #acgh = ArrayCGH(self.input)
+        #ratios = acgh.compute_ratios(ordered=True)
