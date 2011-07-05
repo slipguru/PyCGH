a9c2d5b0d043c043d0157f269260a5e048ee8585
Salvatore Masecchia <devnull@localhost>
2011-07-05 15:49:17 +0200

    added license

--- pycgh/__init__.py
+++ pycgh/__init__.py
@@ -1,3 +1,9 @@
+#-*- coding: utf-8 -*-
+
+# PyCGH - Python utilities for array CGH data analysis
+# Author: Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
+# License: New BSD
+
 from _arrayCGH import ArrayCGH
 from _agilent import AgilentCGH
 from _nimblegen import NimblegenCGH
