b1cae5592d9a8b382f0b614281f9895418f4f325
Salvatore Masecchia <salvatore.masecchia@gmail.com>
2011-09-05 19:27:38 +0200

    tree test

--- bin/synth_cgh.py
+++ bin/synth_cgh.py
@@ -5,6 +5,10 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import numpy2ri
 robjects.conversion.py2ri = numpy2ri.numpy2ri
 
+from mlabwrap import mlab
+mlab.addpath('../spams_release/build/')
+mlab.addpath('../spams_release/test_release/')
+
 from matplotlib import pylab as pl
 
 from scipy.cluster.hierarchy import ward, dendrogram
@@ -51,7 +55,7 @@ FLLat = robjects.r['FLLat']
 #result_pve = FLLat_PVE(X.T, J_seq)
 #PVEs = np.asarray(result_pve.rx2('PVEs'))
 #pl.plot(PVEs, 'o')
-J = 8
+J = 10
 
 ## Best model -----------------------------------------------------------------
 print 'Calcolo modello...'
@@ -63,12 +67,9 @@ result_fflat = FLLat(X.T, J=J, lam1=lam1, lam2=lam2)#, **{'maxiter.T':0})
 D = np.asarray(result_fflat.rx2('Beta'))
 A = np.asarray(result_fflat.rx2('Theta'))
 
-#D = np.asarray(result_bic.rx2('opt.FLLat').rx2('Beta'))
-#A = np.asarray(result_bic.rx2('opt.FLLat').rx2('Theta'))
-
 pl.figure()
 for p, d in enumerate(D.T):
-    pl.subplot(4, 2, p+1)
+    pl.subplot(5, 2, p+1)
     pl.plot(d, '.')
 
 ## Clustering -----------------------------------------------------------------
@@ -98,6 +99,21 @@ pl.colorbar()
 # e nemmeno la sola fase separata di calcolo del dizionario dato A
 # se uso fflat con maxiter.T = 0... dovrebbe tenermi sempre fisso Theta=A
 # va modificata la funzione fflat in modo da accettare old.T oltre a old.B
+param = mlab.struct('loss', 'square',
+                    'regul', 'tree-l2')
+tree = mlab.struct('own_variables', [0, 0, 3, 5, 6, 6, 8, 9],
+                   'N_own_variables', [0, 3, 2, 1, 0, 2, 1, 1],
+                   'eta_g', [1, 1, 1, 2, 2, 2, 2.5, 2.5],
+                   'groups', [[0, 0, 0, 0, 0, 0, 0, 0],
+                              [1, 0, 0, 0, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0, 0, 0, 0],
+                              [1, 0, 0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 1, 0, 0, 0],
+                              [0, 0, 0, 0, 1, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0, 1, 0]])
+
+A, optim_info = mlab.mexFistaTree(X.T,D,A,tree,param)
 
 
 #pl.imshow(X, aspect='auto')
