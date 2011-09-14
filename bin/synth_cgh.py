b243261bc53ad009fd9c30368c04e63447802c87
Salvatore Masecchia <salvatore.masecchia@disi.unige.it>
2011-09-14 11:41:54 +0200

    tree tests updates

--- bin/synth_cgh.py
+++ bin/synth_cgh.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from rpy2 import robjects
@@ -19,114 +21,165 @@ from scipy.cluster.hierarchy import ward, dendrogram
 N = 30              # numero sample
 M = 1000            # numero probe
 S = 3               # numero segmenti
-C = (1., -1., 2.)   # livelli segnale
+C = (1., -1., 1.)   # livelli segnale
 J = (100, 500, 800) # inizio alterazione
-K = (50, 200, 150)   # lunghezza alterazione
+K = (50, 100, 150)  # lunghezza alterazione
+P = 5               # numero variabili latenti
 
-bounds_err = 5.0
+bounds_err = 5.0  # probe
 signal_err = 1e-1
 
-X = np.zeros((N, M))
+#X = np.zeros((N, M))
+rD = np.zeros((M, P))
 
 ## Data generation ------------------------------------------------------------
 print 'Generazione dati...'
-for i, x in enumerate(X):
-    x[:] = np.random.normal(scale=signal_err, size=M)
+X = np.load('synth_cgh_data.npy')
+#for i, x in enumerate(X):
+#    x[:] = np.random.normal(scale=signal_err, size=M)
+#
+#    if i%2 == 0:
+#        str, end = 0, 2
+#    #elif i%3 == 1:
+#        #str, end = 1, 2 # solo centrale
+#    else:
+#        str, end = 1, 3
+#
+#    for c, j, k in zip(C, J, K)[str:end]:
+#
+#        j += int(np.random.normal(scale=bounds_err))
+#        k += int(np.random.normal(scale=bounds_err))
+#
+#        x[j:j+k] += c
+#np.save('synth_cgh_data.npy', X)
 
-    if i%2 == 0:
-        str, end = 0, 2
-    #elif i%3 == 1:
-        #str, end = 1, 2 # solo centrale
-    else:
-        str, end = 1, 3
+print 'Generazione dizionario reale...'
+for p, c, j, k in zip((1, 0, 2), C, J, K):
+    rD[j:j+k,p] += c
 
-    for c, j, k in zip(C, J, K)[str:end]:
-
-        j += int(np.random.normal(scale=bounds_err))
-        k += int(np.random.normal(scale=bounds_err))
-
-        x[j:j+k] += c
-
-
-## Plot data ------------------------------------------------------------------
-pl.figure()
-for p, x in enumerate(X):
-    pl.subplot(6, 5, p+1)
-    pl.plot(x, '.')
-
-## FLLat ----------------------------------------------------------------------
+### FLLat ----------------------------------------------------------------------
 importr('FLLat')
-#FLLat_PVE = robjects.r['FLLat.PVE']
+##FLLat_PVE = robjects.r['FLLat.PVE']
 FLLat_BIC = robjects.r['FLLat.BIC']
 FLLat = robjects.r['FLLat']
-
-## J selection ----------------------------------------------------------------
-#J_seq = robjects.IntVector(range(1, N+1))
-#result_pve = FLLat_PVE(X.T, J_seq)
-#PVEs = np.asarray(result_pve.rx2('PVEs'))
-#pl.plot(PVEs, 'o')
-P = 7
-
+#
+### J selection ----------------------------------------------------------------
+##J_seq = robjects.IntVector(range(1, N+1))
+##result_pve = FLLat_PVE(X.T, J_seq)
+##PVEs = np.asarray(result_pve.rx2('PVEs'))
+##pl.plot(PVEs, 'o')
+#
 ## Best model -----------------------------------------------------------------
 print 'Calcolo modello...'
-result_bic = FLLat_BIC(X.T, J=P, **{'maxiter.T':1}) #nessuna iterazione coeffs
-lam1 = result_bic.rx2('lam1')
-lam2 = result_bic.rx2('lam2')
+result_bic = FLLat_BIC(X.T, J=P)
+lam1 = 1. * result_bic.rx2('lam1')[0]
+lam2 = 1. * result_bic.rx2('lam2')[0]
 
-print lam1, lam2
-
-## Initial coefficients --
-A = np.zeros((P, N))
-
-## Fixed Dictionary (initial **guess**) --
-#D = np.zeros((M, P))
-#for c, j, k, i in zip(C, J, K, (1, 0, 4)):
-    #D[j:j+k, i] += c
-#D = X[np.random.random_integers(0, N-1, P), :].T # Random dictionary
-result_fflat = FLLat(X.T, J=P, lam1=lam1, lam2=lam2, **{'maxiter.T':1})
+## FLLat result --
+D = X[random.sample(xrange(N), P), :].T
+result_fflat = FLLat(X.T, J=P, B=D, lam1=lam1, lam2=lam2, **{'maxiter.T':1})
 D = np.asarray(result_fflat.rx2('Beta'))
-A = np.asarray(result_fflat.rx2('Theta'))
+#A = np.asarray(result_fflat.rx2('Theta'))
+#Xest = np.dot(D, A).T
 
-### Algoritmo ------------------------------------------------------------------
-#for i in xrange(100):
-#
-#    ## SPAMS --
-#    A = mlab.test_FistaTree(X.T, D, A)
-#
-#    # FLLat --
-#    result_fflat = FLLat(X.T, J=P, B=D, T=A, lam1=lam1, lam2=lam2, **{'maxiter.T':0})
-#    D = np.asarray(result_fflat.rx2('Beta'))
-#
-#    #pl.figure()
-#    #for p, d in enumerate(D.T):
-#    #    pl.subplot(4, 2, p+1)
-#    #    pl.plot(d, '.')
+#for a in A:
+    #print np.linalg.norm(a, 2)
 
-#A = mlab.test_FistaTree(X.T, D, A)
-#print np.array(A > 1e-6, dtype=int)
+### Plot data estimations ------------------------------------------------------
+#pl.figure(1)
+#pl.title('FLLat')
+#for p, (x, xest) in enumerate(zip(X, Xest)):
+#    pl.subplot(6, 5, p+1)
+#    pl.plot(x, 'b.')
+#    pl.plot(xest, 'r-', alpha=0.7)
+#
+### Plot dictionary ------------------------------------------------------------
+#pl.figure(2)
+#pl.title('FLLat')
+#for p, d in enumerate(D.T):
+#    pl.subplot(4, 2, p+1)
+#    pl.plot(d, '.')
+#
+### Plot coefficients ----------------------------------------------------------
+#pl.figure(3)
+#pl.title('FLLat')
+#pl.imshow(A, aspect='auto', interpolation='nearest')
+#pl.colorbar()
+#
+#print 'Minimo FLLat', np.linalg.norm(X - Xest, 2)
+
+A = np.zeros((P, N))
+#D = rD.copy()
 
 ## Plot dictionary ------------------------------------------------------------
-pl.figure()
+pl.figure(2)
+pl.title('Starting dictionary')
 for p, d in enumerate(D.T):
     pl.subplot(4, 2, p+1)
     pl.plot(d, '.')
 
-## Clustering atomi -----------------------------------------------------------
-print 'Clustering atomi...'
-linkage = ward(A)
-pl.figure()
-dendrogram(linkage)
+## FLLat + SPAMS results --
+for i in xrange(500):
 
-## Clustering samples ----------------------------------------------------------
-print 'Clustering samples...'
-linkage = ward(A.T)
-pl.figure()
-dendrogram(linkage)
+    # SPAMS --
+    dmean = D.mean(axis=0)
+    Dc = D - dmean
+    A = mlab.test_FistaTree(X.T, Dc, A) # coefficients recalculation
+    norms = np.apply_along_axis(np.linalg.norm, 1, A)
+    norms = np.array([n or 1.0 for n in norms])
+    A /= norms[:, np.newaxis] # limiting weights
 
-pl.figure()
+    dsort = np.argsort(A.sum(axis=1)/N)
+    D = D[:, dsort[::-1][[0, 1, 3, 2, 4]]]
+
+    # FLLat --
+    # proviamo ad iterare anche su A per limitare la norma... hack!
+    result_fflat = FLLat(X.T, J=P, B=D, T=A, lam1=lam1, lam2=lam2,
+                         **{'maxiter.T':0, 'maxiter.B':1, 'maxiter':100}) # coefficients updates ???
+    D = np.asarray(result_fflat.rx2('Beta'))
+    #A = np.asarray(result_fflat.rx2('Theta')) # A limitato
+
+Xest = np.dot(D, A).T
+
+print 'Minimo FLLat+SPAMS', np.linalg.norm(X - Xest, 2)
+print A
+for a in A:
+    print np.linalg.norm(a, 2)
+
+#exit()
+
+## Plot data estimations ------------------------------------------------------
+pl.figure(4)
+pl.title('FLLat + SPAMS')
+for p, (x, xest) in enumerate(zip(X, Xest)):
+    pl.subplot(6, 5, p+1)
+    pl.plot(x, 'b.')
+    pl.plot(xest, 'r-', alpha=0.7)
+
+## Plot dictionary ------------------------------------------------------------
+pl.figure(5)
+pl.title('FLLat + SPAMS')
+for p, d in enumerate(D.T):
+    pl.subplot(4, 2, p+1)
+    pl.plot(d, '.')
+
+## Plot coefficients ----------------------------------------------------------
+pl.figure(6)
+pl.title('FLLat + SPAMS')
 pl.imshow(A, aspect='auto', interpolation='nearest')
 pl.colorbar()
 
+### Clustering atomi -----------------------------------------------------------
+#print 'Clustering atomi...'
+#linkage = ward(A)
+#pl.figure()
+#dendrogram(linkage)
+#
+### Clustering samples ----------------------------------------------------------
+#print 'Clustering samples...'
+#linkage = ward(A.T)
+#pl.figure()
+#dendrogram(linkage)
 
 
 #pl.imshow(X, aspect='auto')
